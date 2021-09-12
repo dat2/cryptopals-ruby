99134ade1ecd2ee44ea42ceed1ef3b6d958b6142
Nick Dujay <nick.dujay@instacart.com>
2021-09-11 23:30:28 -0400

    minor rspec fixes

--- spec/cryptopals_spec.rb
+++ spec/cryptopals_spec.rb
@@ -3,8 +3,8 @@
 
 require 'cryptopals'
 
-RSpec.describe Cryptopals, '#hex_to_base64' do
-  it 'converts hex to base64 correctly' do
+describe Cryptopals, '#to_base64' do
+  it 'converts to base64 correctly' do
     # rubocop:disable Layout/LineLength
     bytes = Cryptopals.to_bytes '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
     # rubocop:enable Layout/LineLength
